ac5b22eca98ce1d210769a7438b0d4741252a515
thi <tharin.tae@carmana.com>
2018-07-13 14:24:16 +0700

    fix dockerfile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import "./App.css";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_SERVER_ENDPOINT
